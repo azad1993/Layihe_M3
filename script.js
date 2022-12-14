f862fad1a78304d7ce32591f557ba15c757b2ca9
azad <azad.shahbazov93@gmail.com>
2022-12-14 22:55:16 +0400

    changes

--- script.js
+++ script.js
@@ -5,71 +5,72 @@ let resultInput = document.querySelector(".second_input");
 let button1 = document.querySelector(".buttons_first");
 let button2 = document.querySelector(".buttons_second");
 
-fetch(`https://api.exchangerate.host/latest?base=${base}&symbols=${symbols}`)
-  .then((res) => res.json())
-  .then((data) => {
-    var val = +Object.values(data.rates);
-    console.log(val, "val");
-    enteringInput.addEventListener("keyup", (e) => {
+function myFunc(e) {
+   fetch(
+    `https://api.exchangerate.host/latest?base=${base}&symbols=${symbols}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      var val = +Object.values(data.rates);
+      console.log(val, "vali");
       let inputVal = +e.target.value;
       let result = val * inputVal;
       resultInput.value = result.toFixed(2);
     });
-  });
+}
 
-  fetch(`https://api.exchangerate.host/latest?base=${symbols}&symbols=${base}`)
-  .then((res) => res.json())
-  .then((data) => {
-    var val = +Object.values(data.rates);
-    console.log(val, "val");
-    resultInput.addEventListener("keyup", (e) => {
+function myFunc2(e) {
+  fetch(`https://api.exchangerate.host/latest?base=${base}&symbols=${symbols}`)
+    .then((res) => res.json())
+    .then((data) => {
+      var val = +Object.values(data.rates);
+      console.log(val, "val");
       let inputVal = +e.target.value;
-      let result = val * inputVal;
+      let result = inputVal / val;
       enteringInput.value = result.toFixed(2);
     });
-  });
+}
+
+function myFunc3() {
+  fetch(`https://api.exchangerate.host/latest?base=${base}&symbols=${symbols}`)
+    .then((res) => res.json())
+    .then((data) => {
+      var val = +Object.values(data.rates);
+      console.log(val, "val");
+      let result = val * enteringInput.value;
+      resultInput.value = result.toFixed(2);
+    });
+}
+
+enteringInput.addEventListener("keyup", (e) => {
+  myFunc(e);
+});
+
+resultInput.addEventListener("keyup", (e) => {
+  myFunc2(e);
+});
 
 Object.values(button1.children).forEach((element) => {
-  
   element.addEventListener("click", (e) => {
-    Object.values(button1.children).forEach((element) => {element.classList.remove('active')})
-    element.classList.toggle('active')
-    
+    Object.values(button1.children).forEach((element) => {
+      element.classList.remove("active");
+    });
+    element.classList.toggle("active");
+
     base = element.innerHTML;
     console.log(base);
-    fetch(
-      `https://api.exchangerate.host/latest?base=${base}&symbols=${symbols}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        var val = +Object.values(data.rates);
-        console.log(val, "val");
-
-        let result = val * enteringInput.value;
-        resultInput.value = result.toFixed(2);
-       
-      });
+    myFunc3();
   });
 });
 
 Object.values(button2.children).forEach((element) => {
   element.addEventListener("click", (e) => {
-    Object.values(button2.children).forEach((element) => {element.classList.remove('active')})
-    element.classList.toggle('active')
+    Object.values(button2.children).forEach((element) => {
+      element.classList.remove("active");
+    });
+    element.classList.toggle("active");
     symbols = element.innerHTML;
     console.log(symbols);
-    fetch(
-      `https://api.exchangerate.host/latest?base=${base}&symbols=${symbols}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        var val = +Object.values(data.rates);
-        console.log(val, "val");
-
-        let result = val * enteringInput.value;
-        resultInput.value = result.toFixed(2);
-       
-      });
+    myFunc3();
   });
 });
-
